08f2cdc5451a1de5375c8f6ec4b44ddd7bff8d8c
manish0705534 <mg0705534@gmail..com>
2026-07-28 14:28:19 +0530

    modify index file

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log("Hello, DevOps!");
 console.log("I'm learning about DevOps pipelines and Git integration.");
+console.log("This is a simple JavaScript file to demonstrate Git version control.");
